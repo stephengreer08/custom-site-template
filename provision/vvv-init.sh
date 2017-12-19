150f59540db874174d9a1c6217ddb9c0f9e20aec
Stephen Greer <info@stephengreer.me>
2017-12-19 06:55:51 -0700

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
+# Provision WordPress Stable
 
-# Add the site name to the hosts file
-echo "127.0.0.1 ${VVV_SITE_NAME}.local # vvv-auto" >> "/etc/hosts"
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
+DOMAINS=`get_hosts "${DOMAIN}"`
+SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
+WP_VERSION=`get_config_value 'wp_version' 'latest'`
+WP_TYPE="subdomain"
+DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
 # Make a database, if we don't already have one
-echo -e "\nCreating database '${VVV_SITE_NAME}' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${VVV_SITE_NAME}"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${VVV_SITE_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
 echo -e "\n DB operations done.\n\n"
-
-echo -e "${VVV_PATH_TO_SITE}"
 
 # Nginx Logs
 mkdir -p ${VVV_PATH_TO_SITE}/log
@@ -17,11 +21,35 @@ touch ${VVV_PATH_TO_SITE}/log/error.log
 touch ${VVV_PATH_TO_SITE}/log/access.log
 
 # Install and configure the latest stable version of WordPress
-cd ${VVV_PATH_TO_SITE}
-if ! $(wp core is-installed --allow-root); then
-  wp core download --path="${VVV_PATH_TO_SITE}" --allow-root
-  wp core config --dbname="${VVV_SITE_NAME}" --dbuser=wp --dbpass=wp --quiet --allow-root
-  wp core multisite-install --url="${VVV_SITE_NAME}.local" --quiet --title="${VVV_SITE_NAME}" --admin_name=admin --admin_email="admin@${VVV_SITE_NAME}.local" --admin_password="password" --allow-root
-else
-  wp core update --allow-root
+if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
+    echo "Downloading WordPress..."
+  noroot wp core download --version="${WP_VERSION}"
 fi
+
+if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
+  echo "Configuring WordPress Stable..."
+  noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+define( 'WP_DEBUG', true );
+PHP
+fi
+
+if ! $(noroot wp core is-installed); then
+  echo "Installing WordPress Stable..."
+
+  if [ "${WP_TYPE}" = "subdomain" ]; then
+    INSTALL_COMMAND="multisite-install --subdomains"
+  elif [ "${WP_TYPE}" = "subdirectory" ]; then
+    INSTALL_COMMAND="multisite-install"
+  else
+    INSTALL_COMMAND="install"
+  fi
+
+  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
+else
+  echo "Updating WordPress Stable..."
+  cd ${VVV_PATH_TO_SITE}/public_html
+  noroot wp core update --version="${WP_VERSION}"
+fi
+
+# cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+# sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
